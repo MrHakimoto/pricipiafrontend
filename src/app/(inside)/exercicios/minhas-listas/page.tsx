35b53673b4d9a1e242cd06041b8df91fe9f2a2bc
MrHakimoto <fodp1116@gmail.com>
2025-11-12 22:20:35 -0300

    muitas correções2

--- src/app/(inside)/exercicios/minhas-listas/page.tsx
+++ src/app/(inside)/exercicios/minhas-listas/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { questions } from "@/utils/db";
+
 import { ModelQuestions } from "@/components/questions/ModelQuestions"
 import { FilterPanel } from "@/components/Panel/PanelFilter"
 import { ExercisesHeader } from "@/components/questions/ExercisesHeader"
